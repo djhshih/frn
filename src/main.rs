f235171ba835e5096d1a8d6fdd1e187c89d51294
David J. H. Shih <djh.shih@gmail.com>
2023-01-10 22:15:37 +0800

    Implement command line interface.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use clap::Parser;
-
-/// Replace pattern in file name
-#[derive(Parser)]
-struct Cli {
-    /// pattern to look for
-    pattern: String,
-    /// path to file
-    path: std::path::PathBuf,
-}
+use clap::{Arg, Command, ArgAction};
 
 fn main() {
-    let args = Cli::parse();
+    let matches = Command::new("frn")
+        .version("0.1")
+        .about("Rename files using regular expression")
+        .arg(
+            Arg::new("regex")
+                .help("substitution specified as a regular expression")
+        )
+        .arg(
+            Arg::new("file")
+                .action(ArgAction::Append)
+                .help("file(s) to rename")
+        )
+        .get_matches();
+
+    let regex = matches.get_one::<String>("regex")
+        .expect("regular expression is required");
+    let files = matches.get_many::<String>("file")
+        .expect("at least one file name is required")
+        .map(|v| v.as_str())
+        .collect::<Vec<_>>();
+    
+    println!("regex: {}", &regex);
+    println!("files: {:?}", &files);
 }
